8d37ae2ba37c2fafafb853a0f92d2df0f5f49587
D4NTE <111151866+D4NTE98@users.noreply.github.com>
2025-04-01 20:19:56 +0200

    Create config.lua

---
+++ config.lua
@@ -0,0 +1,10 @@
+Config = {
+    Encryption = {
+        KeyRotationTime = 24, -- w godzinach
+        EnableMemoryChecks = true
+    },
+    Cache = {
+        MaxSize = 1000,
+        TTL = 300 -- 5 minut
+    }
+}
